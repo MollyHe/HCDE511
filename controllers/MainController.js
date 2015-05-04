9816e31587467a80d2af4131f2802dbf7e9965cc
menglinhe <menglinhe117@gmail.com>
2015-05-03 18:38:19 -0700

    update

---
+++ controllers/MainController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by menglinhe on 5/3/15.
+ */
+
+app.controller('TestCtrl');
+
+
+function TestCtrl($scope){
+    $scope.title="TestTestTest";
+}
---
+++ controllers/app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by menglinhe on 5/3/15.
+ */
